bd14ded9da007463640be3784fa4175b51822904
CodeNofish <1980114953@qq.com>
2024-11-10 16:14:37 +0800

    added

---
+++ Runtime/Util/DisplayUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Fishwork.Core {
+
+  /// <summary>
+  /// 用于访问显示器和屏幕
+  /// </summary>
+  public static class DisplayUtil {
+    /// <summary>
+    /// 获取显示器数量
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int GetDisplayCount() {
+      return Display.displays.Length;
+    }
+
+    /// <summary>
+    /// 获取指定显示器
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Display GetDisplay(int index = 0) {
+      index = Math.Clamp(index, 0, Display.displays.Length - 1);
+      return Display.displays[index];
+    }
+
+    /// <summary>
+    /// 激活指定显示器
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Activate(int index = 0) {
+      GetDisplay(index).Activate();
+    }
+
+    /// <summary>
+    /// 激活全部显示器
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ActivateAll() {
+      foreach (var display in Display.displays)
+        display.Activate();
+    }
+  }
+
+}
---
+++ Runtime/Util/ScreenUtil.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Fishwork.Core {
+  
+  public static class ScreenUtil {
+    /// <summary>
+    /// 设置屏幕分辨率
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SetResolution(int width, int height, bool fullscreen) {
+      Screen.SetResolution(width, height, fullscreen);
+    }
+
+    /// <summary>
+    /// 设置为竖屏
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SetOrientationPortrait() {
+      Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    /// <summary>
+    /// 设置为横屏
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SetOrientationLandscape() {
+      Screen.orientation = ScreenOrientation.AutoRotation;
+      Screen.autorotateToLandscapeRight = true;
+      Screen.autorotateToLandscapeLeft = true;
+      Screen.autorotateToPortrait = false;
+      Screen.autorotateToPortraitUpsideDown = false;
+    }
+    
+    /// <summary>
+    /// 设置为自动旋转
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SetOrientationAutorotate() {
+      Screen.orientation = ScreenOrientation.AutoRotation;
+      Screen.autorotateToLandscapeRight = true;
+      Screen.autorotateToLandscapeLeft = true;
+      Screen.autorotateToPortrait = true;
+      Screen.autorotateToPortraitUpsideDown = true;
+    }
+  }
+
+  public enum ScreenResolution {
+    Auto,
+    _2048_1536,
+    _1920_1440,
+    _1920_1200,
+    _1920_1080,
+    _1680_1050,
+    _1600_1200,
+    _1600_900,
+    _1440_900,
+    _1400_1050,
+    _1366_768,
+    _1280_1024,
+    _1280_960,
+    _1280_800,
+    _1280_720,
+    _1024_768,
+    _800_600,
+  }
+  
+}
