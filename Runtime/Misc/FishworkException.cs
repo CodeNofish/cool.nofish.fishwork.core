1d04576f9a8a3a20ae3d27dd50f2a0d4656123d9
CodeNofish <1980114953@qq.com>
2024-11-14 13:33:49 +0800

    update

---
+++ Runtime/Misc/FishworkException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Fishwork.Core {
+
+  /// <summary>
+  /// Fishwork内部异常的基类
+  /// </summary>
+  public abstract class FishworkException : Exception { }
+
+}
---
+++ Runtime/Util/ApplicationUtil.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Fishwork.Core {
+
+  public static class ApplicationUtil {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SetFrameRate(int frameRate) {
+      Application.targetFrameRate = frameRate;
+    }
+  }
+
+}
