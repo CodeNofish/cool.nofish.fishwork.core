fcfb29e4e446c7175888dc1bdb2a459ae296e25b
CodeNofish <1980114953@qq.com>
2024-11-14 18:25:54 +0800

    updated

---
+++ Runtime/Extension/Collection/EnumerableExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Fishwork.Core {
+  
+  [PublicAPI]
+  public static class EnumerableExtension {
+    /// <summary>
+    /// 获取IEnumerable元素数量，优先使用非枚举方法
+    /// </summary>
+    public static int GetCount<T>(this IEnumerable<T> source) {
+      return source switch {
+        ICollection<T> collection => collection.Count,
+        ICollection collection2 => collection2.Count,
+        _ => source.Count()
+      };
+    }
+  }
+
+}
